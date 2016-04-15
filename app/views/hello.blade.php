4d72af227b6e9cdce22f4a73e1f830e378f2fbcd
linux <arjunkoid@gmail.com>
2016-04-15 13:10:52 +0545

    Master layout setup

---
+++ app/views/hello.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+	<div class="row">
+    <p>This is my body content.</p>
+    </div>
+@stop
---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset= "UTF-8" >
+		<title>My Website</title>
+		
+		<!-- CSS -->
+		<!-- <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" > -->
+
+		{{HTML::style('css/bootstrap.css') }}
+		{{HTML::script('js/jquery-1.12.3.js')}}
+		{{HTML::script('js/bootstrap.js')}}
+
+		<!-- js -->
+		<!-- <script src= "https://code.jquery.com/jquery.js" ></script> -->
+		<!-- <script src= "//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+ -->
+	</head>
+    <body>
+
+        <div class="container">
+        	<div class="jumbotron">
+            @yield('content')
+            </div>
+        </div>
+    </body>
+</html>
